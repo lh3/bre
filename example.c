be5dcb72af048317dcbd1866cf8784b128afbf20
Heng Li <lh3@me.com>
2024-10-26 07:19:44 +0800

    comment on example.c

--- example.c
+++ example.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 	bre_hdr_init(&hdr, BRE_AT_DNA6, 2);
 	f = bre_open_write(fn, &hdr);
 	bre_write(f, 4, 10);
-	bre_write(f, 4, 100);
-	bre_write(f, 3, 70000);
+	bre_write(f, 4, 100); // this will be combined with the previous run
+	bre_write(f, 3, 70000); // this run will be split as it is longer than 65535
 	bre_close(f);
 
 	// reading
